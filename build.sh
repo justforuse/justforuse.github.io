9443971f7b811a6f3427390376c6e0abb4186eab
justforuse <allenforuse@gmail.com>
2019-11-06 23:52:19 +0800

    update sw

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 hugo -d blog
-mv blog/res/sw.js blog/zh-cn/
+
+echo 'Copying service worker file...'
+
+mv blog/res/sw-v2.js blog/zh-cn/
+
+echo 'Copying manifest file...'
 mv blog/res/manifest.json blog/zh-cn/
